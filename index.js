8f523b64daa66502c5cd656b46d6d03b757d9029
jashonmichael <jashon.michael@student.moringaschool.com>
2024-03-21 13:26:49 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+const calculatesFarePrice = (start, destination) => {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  };
